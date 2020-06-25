ae190477c950abb9d5d73ad8e7f3b96f16ae1bc1
Robynn Ding <hbkxmqdjn@gmail.com>
2020-06-25 16:04:47 -0400

    Updates

---
+++ precache-manifest.c22641d29a67f9b4a943f5b2a6c91966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef1b976b20fb40e073628f933981abd2",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "2a7b4648d8302ac601d7",
+    "url": "/expense-tracker/static/css/main.f0a2ab0d.chunk.css"
+  },
+  {
+    "revision": "b7395f4815d2322be0e5",
+    "url": "/expense-tracker/static/js/2.0d1322a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.0d1322a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a7b4648d8302ac601d7",
+    "url": "/expense-tracker/static/js/main.e2fa2468.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  }
+]);
